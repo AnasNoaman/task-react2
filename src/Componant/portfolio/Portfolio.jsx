def90b7e37c74939f86bff42aee0ad9bd9674a42
Anas Noaman <147315220+AnasNoaman@users.noreply.github.com>
2025-04-28 23:11:35 +0300

    first commit

--- src/Componant/portfolio/Portfolio.jsx
+++ src/Componant/portfolio/Portfolio.jsx
@@ -1,4 +1,3 @@
-import React, { useEffect, useState } from 'react'
 import style from './Portfolio.module.css'
 import img1 from '../../assets/cabin.png'
 import img2 from '../../assets/cake.png'
@@ -10,40 +9,37 @@ export default function Portfolio() {
   
   return (
     <section className="py-5">
-        
-        <div className="container">
-        
+  <div className="container">
     <div className="portfolio-divider d-flex align-items-center justify-content-center mb-5 g-5 flex-column">
       <h2 className=" fw-bold mb-2">PORTFOLIO</h2>
-      <span className="portfolio-star mx-3">&#9733;</span>
+      <span className="portfolio-star mx-3">★</span>
     </div>
-        <div class=" text-center d-flex flex-column g-5">
-  <div class="row align-items-center d-flex gap-3 ">
-    <div class="col">
-      <img className={` ${style.imgw}`} src={img1}/>
-    </div>
-    <div class="col">
-      <img className={` ${style.imgw}`} src={img2}/>
-    </div>
-    <div class="col">
-    <img className={` ${style.imgw}`} src={img3}/>
-    </div>
-  </div>
-  <div class="row align-items-center pt-5 d-flex gap-3">
-    <div class="col">
-    <img className={` ${style.imgw}`} src={img4}/>
-    </div>
-    <div class="col">
-    <img className={` ${style.imgw}`} src={img5}/>
-    </div>
-    <div class="col">
-    <img className={` ${style.imgw}`} src={img6}/>
-    </div>
-  </div>
-</div>
+    <div className=" text-center d-flex flex-column g-5">
+      <div className="row align-items-center d-flex gap-3 ">
+        <div className="col">
+          <img className={` ${style.imgw}`} src={img1} />
         </div>
-       
-    </section>
+        <div className="col">
+          <img className={` ${style.imgw}`} src={img2} />
+        </div>
+        <div className="col">
+        <img className={` ${style.imgw}`} src={img3} />
+        </div>
+      </div>
+      <div className="row align-items-center pt-5 d-flex gap-3">
+        <div className="col">
+        <img className={` ${style.imgw}`} src={img4} />
+        </div>
+        <div className="col">
+        <img className={` ${style.imgw}`} src={img5} />
+        </div>
+        <div className="col">
+        <img className={` ${style.imgw}`} src={img6} />
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
    
   )
 }
